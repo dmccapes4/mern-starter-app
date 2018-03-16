d4c8a09966d093432871083303d5f562f160bde6
Dylan McCapes <dylanmccapes@Dylans-MacBook-Pro.local>
2018-03-16 12:28:46 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,54 @@
+import express from 'express'
+import path from 'path'
+import bodyParser from 'body-parser'
+import logger from 'morgan'
+import mongoose from 'mongoose'
+import bb from 'express-busboy'
+
+// import routes
+import routes from './routes/router.server.route'
+
+// set up app with express
+const app = express()
+
+// express-busboy to parse multipart/form-data
+bb.extend(app)
+
+// allow cross-origin resource sharing
+app.use ((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "*")
+    res.header("Access-Control-Allow-Headers", "Origin", "X-Requested-With, Content-Type, Accept")
+    next()
+})
+
+// configure app
+app.use(logger('dev'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended:true }))
+app.use(express.static(path.join(__dirname, 'public')))
+
+// set the port
+const port = process.env.PORT || 3001
+
+// connect to database
+mongoose.Promise = global.Promise
+mongoose.connect('mongodb://localhost/mern-app', {
+    useMongoClient: true
+})
+
+app.use('/api', routes)
+
+app.get('/', (req, res) => {
+    return res.end('Api working')
+})
+
+// catch 404
+app.use((req, res, next) => {
+    res.status(404).send('<h2 align=center>page not found?</h2>')
+})
+
+// start the server
+app.listen(port, () => {
+    console.log(`app server listening at ${port}`);
+    
+})
